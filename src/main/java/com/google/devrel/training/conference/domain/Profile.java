044dbf140181cd1c52f00f1a98d90cd094fc53e4
Stas Telizhenko <alex@alexcp.com>
2018-02-27 23:28:20 +0200

    test

---
+++ src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -0,0 +1,56 @@
+package com.google.devrel.training.conference.domain;
+
+import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+
+@Entity
+public class Profile {
+	String displayName;
+	String mainEmail;
+	TeeShirtSize teeShirtSize;
+
+	@Id String userId;
+
+	/**
+	 * Public constructor for Profile.
+	 *
+	 * @param userId       The user id, obtained from the email
+	 * @param displayName  Any string user wants us to display him/her on this system.
+	 * @param mainEmail    User's main e-mail address.
+	 * @param teeShirtSize The User's tee shirt size
+	 */
+	public Profile(String userId, String displayName, String mainEmail, TeeShirtSize teeShirtSize) {
+		this.userId = userId;
+		this.displayName = displayName;
+		this.mainEmail = mainEmail;
+		this.teeShirtSize = teeShirtSize;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public String getMainEmail() {
+		return mainEmail;
+	}
+
+	public TeeShirtSize getTeeShirtSize() {
+		return teeShirtSize;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void update(String displayName, TeeShirtSize teeShirtSize){
+		this.displayName = displayName;
+		this.teeShirtSize = teeShirtSize;
+	}
+	/**
+     * Just making the default constructor private.
+     */
+    private Profile() {}
+
+}
